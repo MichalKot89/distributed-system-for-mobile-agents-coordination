2ea9e33f9ed9dafa5d9f518a32db7b4f64983bf9
wcisk88 <wcisk88@235605e8-0642-d884-9e11-1408ab9363fe>
2011-10-15 16:05:38 +0000

    Pliki testowe do wywalenia jesli trzeba

---
+++ src/test.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 14-10-2011
+ *      Author: michal
+ */
+#include <iostream>
+#include "main.hh"
+#include "test.hh"
+
+using namespace std;
+
+int test()
+{
+cout<<GROUP<<endl;
+return 1;
+
+
+}
+
---
+++ src/test.hh
@@ -0,0 +1,7 @@
+
+
+#ifndef TEST_HH_
+#define TEST_HH_
+
+int test(void);
+#endif /* MAIN_HH_ */
